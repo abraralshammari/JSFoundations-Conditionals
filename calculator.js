ab70cd32164a1b785590ec9fa080e159bd917636
Abrar <abrar@Abrars-MacBook-Pro.local>
2021-02-02 18:56:56 +0300

    solved

--- calculator.js
+++ calculator.js
@@ -1,3 +1,15 @@
 // Define the numbers and the operation here
+let num0=1
+let num1=2
+let str="-"
 
 // Write if-statements here
+
+if ( str=== "-")
+{
+    console.log( num0 - num1 )
+}
+
+else {
+    console.log("false")
+}
